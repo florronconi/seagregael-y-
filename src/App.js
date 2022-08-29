5476d19f38894a71aa4045cc27e48c32c2598c1f
florronconi <florronconi66@gmail.com>
2022-08-29 17:24:37 +0200

    se agrega +y-

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './modulos/Navbar.js';
+import ItemList from './modulos/ItemListContainer';
+
+function App() {
+  return (
+    <>
+    <Navbar  />
+      
+    <hr/>
+    <ItemList />
+
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/modulos/ItemListContainer.js
@@ -0,0 +1,15 @@
+import ItemCount from './ItemCount'
+
+
+const ItemList =() => {
+
+    return ( 
+            <>
+                <hr/>
+                <ItemCount stock={5} initial={1}/>
+            </>
+
+    )
+}
+
+export default ItemList
